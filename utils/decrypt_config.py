7144a4cdaa1f764ba30a7ca6ce3a63726f289e26
FoxeiZ <foxeiz2004@gmail.com>
2024-05-14 20:35:41 +0700

    run on main

--- utils/decrypt_config.py
+++ utils/decrypt_config.py
@@ -1,27 +1,28 @@
 import os
 from cryptography.fernet import Fernet
 
-if not os.path.exists("config.encrypted"):
-    raise FileNotFoundError("config.encrypted not found")
+if __name__ == "__main__":
+    if not os.path.exists("config.encrypted"):
+        raise FileNotFoundError("config.encrypted not found")
 
-if os.path.exists("key.key"):
-    with open("key.key", "rb") as key_file:
-        key = key_file.read()
-else:
-    key = os.environ.get("KEY", "").encode()
+    if os.path.exists("key.key"):
+        with open("key.key", "rb") as key_file:
+            key = key_file.read()
+    else:
+        key = os.environ.get("KEY", "").encode()
 
-if not key:
-    raise ValueError("No value of KEY has been provided")
+    if not key:
+        raise ValueError("No value of KEY has been provided")
 
-fernet = Fernet(key)
+    fernet = Fernet(key)
 
-with open("key.key", "wb") as key_file:
-    key_file.write(key)
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
 
-with open("config.encrypted", "rb") as config_file:
-    encrypted_config = config_file.read()
-    decrypted_config = fernet.decrypt(encrypted_config)
+    with open("config.encrypted", "rb") as config_file:
+        encrypted_config = config_file.read()
+        decrypted_config = fernet.decrypt(encrypted_config)
 
-# print(decrypted_config)
-with open("config.json", "wb") as config_file:
-    config_file.write(decrypted_config)
+    # print(decrypted_config)
+    with open("config.json", "wb") as config_file:
+        config_file.write(decrypted_config)
--- utils/encrypt_config.py
+++ utils/encrypt_config.py
@@ -1,21 +1,22 @@
 import os
 from cryptography.fernet import Fernet
 
-if not os.path.exists("config.json"):
-    raise FileNotFoundError("config.json not found")
+if __name__ == "__main__":
+    if not os.path.exists("config.json"):
+        raise FileNotFoundError("config.json not found")
 
-if os.path.exists("key.key"):
-    with open("key.key", "rb") as key_file:
-        key = key_file.read()
-else:
-    key = Fernet.generate_key()
+    if os.path.exists("key.key"):
+        with open("key.key", "rb") as key_file:
+            key = key_file.read()
+    else:
+        key = Fernet.generate_key()
 
-fernet = Fernet(key)
+    fernet = Fernet(key)
 
-with open("key.key", "wb") as key_file:
-    key_file.write(key)
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
 
-with open("config.json", "rb") as config_file:
-    encrypted_config = fernet.encrypt(config_file.read())
-    with open("config.encrypted", "wb") as config_file:
-        config_file.write(encrypted_config)
+    with open("config.json", "rb") as config_file:
+        encrypted_config = fernet.encrypt(config_file.read())
+        with open("config.encrypted", "wb") as config_file:
+            config_file.write(encrypted_config)
